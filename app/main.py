564ad38a5a731bda5fb43d7e4c6d53046578651b
benjamin <benjamin.ghailane@gmail.com>
2025-04-20 23:25:58 +0200

    edited deploy_api.yml

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ async def predict(image: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("appp.main:appp", host="127.0.0.1", port=8000)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000)
